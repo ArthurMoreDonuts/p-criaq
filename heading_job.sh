6615eb0c915002b6e56e6e4d8a4268467e1f0f65
ArthurMoreDonuts <ahmed_hani_dawoud@hotmail.com>
2025-10-27 08:00:22 -0400

    Added different jobs

--- heading_job.sh
+++ heading_job.sh
@@ -1,8 +1,17 @@
 #!/bin/bash
+#SBATCH --job-name=Heading
 #SBATCH --account=def-yangw-ab
 #SBATCH --gpus-per-node=1
 #SBATCH --mem=16000M               # memory per node
 #SBATCH --time=0-24:00
 #SBATCH --mail-user=ahmed_hani_dawoud@hotmail.com
 #SBATCH --mail-type=ALL
+
+# --- Activate Virtual Environment ---
+echo "Activating python virtual environment"
+source ~/py312/bin/activate
+
+# --- Navigate to Project Directory and Run Script
+echo "Changing to the project directory..."
+cd /home/o7ahmed/scratch/p-craiq
 python heading.py
