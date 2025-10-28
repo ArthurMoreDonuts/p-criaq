5b96a51b73706074d4a57d916a59cd7606075d8c
ArthurMoreDonuts <ahmed_hani_dawoud@hotmail.com>
2025-10-28 11:13:51 -0400

    Added different jobs

--- altitude_job.sh
+++ altitude_job.sh
@@ -2,7 +2,7 @@
 #SBATCH --job-name=Altitude
 #SBATCH --account=def-yangw-ab
 #SBATCH --gpus-per-node=1
-#SBATCH --mem=16000M               # memory per node
+#SBATCH --mem=32000M               # memory per node
 #SBATCH --time=0-24:00
 #SBATCH --mail-user=ahmed_hani_dawoud@hotmail.com
 #SBATCH --mail-type=ALL
--- fuelflow.py
+++ fuelflow.py
@@ -55,12 +55,7 @@ val_loader = DataLoader(val_dataset, batch_size=bsz,
 #models = [ 'efficientnet_b0.ra_in1k','efficientvit_b0.r224_in1k', 'nextvit_small.bd_in1k','convnext_small.in12k', 'convnextv2_base.fcmae_ft_in1k',  'convit_base.fb_in1k',    'resnetv2_50x1_bit.goog_distilled_in1k',    'beit_base_patch16_224.in22k_ft_in22k',  'cspresnet50.ra_in1k', 'deit3_base_patch16_224.fb_in1k', 'densenet121.ra_in1k', 'dla34.in1k',  'edgenext_base.in21k_ft_in1k','efficientformer_l1.snap_dist_in1k','efficientformerv2_l.snap_dist_in1k', 'fastvit_ma36.apple_dist_in1k',  'gcvit_base.in1k', 'ghostnet_100.in1k','ghostnetv2_100.in1k', 'gmlp_s16_224.ra3_in1k','hgnet_base.ssld_in1k','hrnet_w18.ms_aug_in1k','inception_next_base.sail_in1k','lambda_resnet26t.c1_in1k', 'levit_256.fb_dist_in1k','mobilenetv1_100.ra4_e3600_r224_in1k','mobilenetv2_120d.ra_in1k','mobilenetv3_small_075.lamb_in1k','mobilenetv4_conv_medium.e500_r224_in1k','mobileone_s2.apple_in1k','mobilevit_s.cvnets_in1k','mobilevitv2_200.cvnets_in1k','nasnetalarge.tf_in1k','nest_base_jx.goog_in1k','nf_resnet50.ra2_in1k','nf_regnet_b1.ra2_in1k','nfnet_l0.ra2_in1k','pit_b_224.in1k','pnasnet5large.tf_in1k','poolformer_m36.sail_in1k','pvt_v2_b0.in1k','rdnet_base.nv_in1k','regnetv_040.ra3_in1k','repghostnet_050.in1k','repvgg_a0.rvgg_in1k','repvit_m0_9.dist_300e_in1k','resmlp_12_224.fb_distilled_in1k','sam2_hiera_base_plus.fb_r896','samvit_base_patch16.sa1b','selecsls42b.in1k','seresnet50.a1_in1k','twins_pcpvt_base.in1k','visformer_small.in1k','vit_base_patch8_224.augreg2_in21k_ft_in1k','vit_base_patch32_clip_quickgelu_224.metaclip_400m','vitamin_small_224.datacomp1b_clip', 'xcit_small_24_p16_224.fb_in1k','skresnet18.ra_in1k','swin_base_patch4_window7_224.ms_in1k','tf_efficientnet_b0.aa_in1k','tiny_vit_5m_224.in1k',]
 
 
-models = [  'coat_small.in1k' ,
-            'deit_base_patch16_224.fb_in1k',
-            'convnext_base.fb_in22k',
-            'dpn68.mx_in1k',
-            'focalnet_base_lrf.ms_in1k',
-            'mambaout_base.in1k',
+models = [  
             'maxvit_base_tf_224.in1k',
             'mvitv2_base.fb_in1k',
             'nextvit_base.bd_in1k',
